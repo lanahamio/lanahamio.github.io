4522a5ba1f871a54441905bbfc4d6504e6ef9716
lanahamio <39987217+lanahamio@users.noreply.github.com>
2020-04-19 10:48:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,66 @@
-// Bouncing DVD Logo
-// Daniel Shiffman
-// https://thecodingtrain.com/CodingChallenges/131-bouncing-dvd-logo.html
-// https://youtu.be/0j86zuqqTlQ
-// https://editor.p5js.org/codingtrain/sketches/Ya1K1ngtFk
+let speed = 20;
+let scale = 0.17; // Image scale (I work on 1080p monitor)
+let canvas;
+let ctx;
+let logoColor;
 
-let x;
-let y;
+let dvd = {
+    x: 200,
+    y: 300,
+    xspeed: 10,
+    yspeed: 10,
+    img: new Image()
+};
 
-let xspeed;
-let yspeed;
+(function main(){
+    canvas = document.getElementById("tv-screen");
+    ctx = canvas.getContext("2d");
+    dvd.img.src = 'neenaa_DVD_logo.png';
 
-let dvd;
+    //Draw the "tv screen"
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-let r, g, b;
+    pickColor();
+    update();
+})();
 
-function preload() {
-  dvd = loadImage('neenaa_DVD_logo.png');
+function update() {
+    setTimeout(() => {
+        //Draw the canvas background
+        ctx.fillStyle = '#000';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        //Draw DVD Logo and his background
+        ctx.fillStyle = logoColor;
+        ctx.fillRect(dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
+        ctx.drawImage(dvd.img, dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
+        //Move the logo
+        dvd.x+=dvd.xspeed;
+        dvd.y+=dvd.yspeed;
+        //Check for collision 
+        checkHitBox();
+        update();   
+    }, speed)
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  x = random(width);
-  y = random(height);
-  xspeed = 10;
-  yspeed = 10;
-  pickColor();
+//Check for border collision
+function checkHitBox(){
+    if(dvd.x+dvd.img.width*scale >= canvas.width || dvd.x <= 0){
+        dvd.xspeed *= -1;
+        pickColor();
+    }
+        
+    if(dvd.y+dvd.img.height*scale >= canvas.height || dvd.y <= 0){
+        dvd.yspeed *= -1;
+        pickColor();
+    }    
 }
 
-function pickColor() {
-  r = random(100, 256);
-  g = random(100, 256);
-  b = random(100, 256);
-}
+//Pick a random color in RGB format
+function pickColor(){
+    r = Math.random() * (254 - 0) + 0;
+    g = Math.random() * (254 - 0) + 0;
+    b = Math.random() * (254 - 0) + 0;
 
-function draw() {
-  background(0);
-  // rect(x, y, 80, 60);
-  // Draw the DVD logo
-  tint(r, g, b);
-  image(dvd, x, y);
-
-  x = x + xspeed;
-  y = y + yspeed;
-
-  if (x + dvd.width >= width) {
-    xspeed = -xspeed;
-    x = width - dvd.width;
-    pickColor();
-  } else if (x <= 0) {
-    xspeed = -xspeed;
-    x = 0;
-    pickColor();
-  }
-
-  if (y + dvd.height >= height) {
-    yspeed = -yspeed;
-    y = height - dvd.height;
-    pickColor();
-  } else if (y <= 0) {
-    yspeed = -yspeed;
-    y = 0;
-    pickColor();
-  }
+    logoColor = 'rgb('+r+','+g+', '+b+')';
 }
